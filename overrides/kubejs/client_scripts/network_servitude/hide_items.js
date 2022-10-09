9ab7aa64f27aa6790c82d116c814ccf3efb680b2
Silly Goose <sillygoose078@gmail.com>
2022-10-09 14:07:45 -0400

    Removed extended crafting tabls Removed basic, advanced, and elite crafting tables and auto crafters.

---
+++ overrides/kubejs/client_scripts/network_servitude/hide_items.js
@@ -0,0 +1,11 @@
+onEvent('jei.hide.items', event => {
+    // extendedcrafting
+    event.hide('extendedcrafting:basic_table')
+    event.hide('extendedcrafting:advanced_table')
+    event.hide('extendedcrafting:elite_table')
+
+    event.hide('extendedcrafting:basic_auto_table')
+    event.hide('extendedcrafting:advanced_auto_table')
+    event.hide('extendedcrafting:elite_auto_table')
+})
+
