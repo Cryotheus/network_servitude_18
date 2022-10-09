e7668b21a2d92fea93d909cb7713fff939e373fd
Cryotheus <14439112+Cryotheus@users.noreply.github.com>
2022-10-08 18:49:43 -0600

    Disable initial_items.js item removals for performance reasons

--- overrides/kubejs/server_scripts/network_servitude/initial_items.js
+++ overrides/kubejs/server_scripts/network_servitude/initial_items.js
@@ -1,8 +1,3 @@
-var disabled_initial_spawns = [
-	//'byg:biomepedia',
-]
-
-var disabled_count = disabled_initial_spawns.length
 var standard_issue_nbt = '{display:{Lore:["{\\"text\\":\\"§7Standard issue for Network Serfs§r\\"}"]}}'
 
 function color_random() {return Math.floor(Math.random() * 16777215)}
@@ -20,44 +15,11 @@ function grant_items(player) {
 	inventory.set(3, Item.of('minecraft:iron_sword', standard_issue_nbt))
 	
 	//inventory.set(7, Item.of('6x extendedmushrooms:mushroom_bread', '{display:{Name:"{\\"text\\":\\"§fBRED 46a-2§r\\"}",Lore:["{\\"text\\":\\"§7Expired... but still edible§r\\"}","{\\"text\\":\\"§7Standard issue for Network Serfs§r\\"}"]}}'))
-	//inventory.set(8, Item.of('sophisticatedbackpacks:backpack', "{borderColor:" + color_random() + ",clothColor:" + color_random() + "}"))
+	inventory.set(7, Item.of('8x pamhc2foodextended:driedsoupitem', '{display:{Name:"{\\"text\\":\\"§fMeat Supplement v2.4.19d (Revision A)§r\\"}",Lore:["{\\"text\\":\\"§7Best described as a \\\\\\"Jerky Brick\\\\\\"§r\\"}","{\\"text\\":\\"§7Standard issue for Network Serfs§r\\"}"]}}'))
+	inventory.set(8, Item.of('sophisticatedbackpacks:backpack', "{borderColor:" + color_random() + ",clothColor:" + color_random() + "}"))
 	
 	//inventory
 	//inventory.set(9, '2x lootbagmod:lootbag')
-	//inventory.set(10, Item.of('4x pamhc2foodextended:driedsoupitem', '{display:{Name:"{\\"text\\":\\"§fMeat Supplement v2.4.19d (Revision A)§r\\"}",Lore:["{\\"text\\":\\"§7Best described as a \\\\\\"Jerky Brick\\\\\\"§r\\"}","{\\"text\\":\\"§7Standard issue for Network Serfs§r\\"}"]}}'))
-}
-
-function remove_items(player) {
-	var march = 0
-	var inventory = player.inventory
-	var stages = player.stages
-	var stage_tracker = {}
-	
-	disabled_initial_spawns.forEach(id => {
-		var slot = inventory.find(Item.of(id).ignoreNBT())
-		var stage_id = 'network_servitude_initial_removals_' + id
-		
-		if (stages.has(stage_id) && !stage_tracker[stage_id])
-		{
-			march++
-			stage_tracker[stage_id] = true
-		}
-		else if (slot > -1) {
-			var item = inventory.get(slot)
-			
-			inventory.clear(item)
-			
-			if (!stage_tracker[stage_id])
-			{
-				march++
-				stage_tracker[stage_id] = true
-				
-				stages.add(stage_id)
-			}
-		}
-	})
-	
-	if (march >= disabled_count) {stages.add("network_servitude_initial_removals")}
 }
 
 onEvent('ftbquests.completed.initial_items', event => {
@@ -65,28 +27,6 @@ onEvent('ftbquests.completed.initial_items', event => {
 	
 	if (player) {grant_items(player)}
 })
-
-onEvent('ftbquests.completed.initial_items_removal', event => {
-	var player = event.player
-	var stages = player.stages
-	
-	if (player) {
-		//disabled_initial_spawns.forEach(id => {stages.remove("network_servitude_initial_removals_" + id)})
-		
-		//stages.remove("network_servitude_initial_removals")
-		//remove_items(player)
-	}
-})
-
-/*
-onEvent('player.inventory.changed', event => {
-	var player = event.player
-	var stages = player.stages
-	
-	if (stages.has("network_servitude_initial_items") && !stages.has("network_servitude_initial_removals")) {
-		//remove_items(player)
-	}
-})*/
 
 onEvent("player.logged_in", event => {
 	if (!event.player.stages.has("network_servitude_initial_items")) {
