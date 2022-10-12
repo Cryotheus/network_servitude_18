f4c32ac90707cc28c333bdeb1bc8536b399a5dcd
Cryotheus <14439112+Cryotheus@users.noreply.github.com>
2022-10-11 20:48:09 -0600

    Migrated Avaritia recipes to Extended Crafting

--- overrides/kubejs/server_scripts/network_servitude/recipe_removal.js
+++ overrides/kubejs/server_scripts/network_servitude/recipe_removal.js
@@ -1,15 +1,21 @@
-onEvent('recipes', event => {
-	event.remove({id: 'jaopca:mekanism.raw_storage_block_to_dirty_slurry.aluminum'})
+//this file is only for recipes that cannot be removed using the data pack
+onEvent("recipes", event => {
+	//extended crafting
+	event.remove({id: "extendedcrafting:aluminum_singularity"})
+	event.remove({id: "extendedcrafting:platinum_singularity"})
 	
-	event.remove({id: 'jaopca:mekanism.ore_to_dirty_slurry.cobalt'})
-	event.remove({id: 'jaopca:mekanism.raw_material_to_dirty_slurry.cobalt'})
-	event.remove({id: 'jaopca:mekanism.raw_storage_block_to_dirty_slurry.cobalt'})
+	//jaopca
+	event.remove({id: "jaopca:mekanism.raw_storage_block_to_dirty_slurry.aluminum"})
 	
-	event.remove({id: 'jaopca:mekanism.ore_to_dirty_slurry.nickel'})
-	event.remove({id: 'jaopca:mekanism.raw_material_to_dirty_slurry.nickel'})
-	event.remove({id: 'jaopca:mekanism.raw_storage_block_to_dirty_slurry.nickel'})
+	event.remove({id: "jaopca:mekanism.ore_to_dirty_slurry.cobalt"})
+	event.remove({id: "jaopca:mekanism.raw_material_to_dirty_slurry.cobalt"})
+	event.remove({id: "jaopca:mekanism.raw_storage_block_to_dirty_slurry.cobalt"})
 	
-	event.remove({id: 'jaopca:mekanism.ore_to_dirty_slurry.silver'})
-	event.remove({id: 'jaopca:mekanism.raw_material_to_dirty_slurry.silver'})
-	event.remove({id: 'jaopca:mekanism.raw_storage_block_to_dirty_slurry.silver'})
+	event.remove({id: "jaopca:mekanism.ore_to_dirty_slurry.nickel"})
+	event.remove({id: "jaopca:mekanism.raw_material_to_dirty_slurry.nickel"})
+	event.remove({id: "jaopca:mekanism.raw_storage_block_to_dirty_slurry.nickel"})
+	
+	event.remove({id: "jaopca:mekanism.ore_to_dirty_slurry.silver"})
+	event.remove({id: "jaopca:mekanism.raw_material_to_dirty_slurry.silver"})
+	event.remove({id: "jaopca:mekanism.raw_storage_block_to_dirty_slurry.silver"})
 })
